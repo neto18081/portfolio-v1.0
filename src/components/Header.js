c701e448b80e268978b0c0cb191c995ae841b684
Pedro <web@agenciaade.com.br>
2022-10-20 20:22:49 -0300

    done/header

---
+++ src/components/Header.js
@@ -0,0 +1,69 @@
+import { SiJavascript } from "react-icons/si";
+
+import Burger from "@animated-burgers/burger-squeeze";
+import "@animated-burgers/burger-squeeze/dist/styles.css";
+import { useState } from "react";
+
+export default function Header() {
+  const [burger, setBurger] = useState(false);
+  return (
+    <>
+      <div className="tw-flex tw-items-center tw-justify-center tw-w-full tw-px-[20px] tw-h-[70px] tw-shadow-md tw-fixed">
+        <div className="tw-flex tw-items-center tw-justify-between tw-w-full tw-mx-auto tw-max-w-[1280px]">
+          <div className="tw-flex tw-items-center tw-gap-[10px]">
+            <span className="tw-font-medium tw-text-[22px]">Pedro</span>
+            <SiJavascript color="#ffdf00" size={25} />
+          </div>
+
+          <ul
+            className={`tw-flex tw-items-center tw-justify-center tw-gap-[20px] tw-fixed md:tw-static tw-top-[70px] tw-right-0 tw-flex-col tw-h-[calc(100vh-70px)] tw-w-full md:tw-h-auto md:tw-w-auto md:tw-flex-row tw-bg-theme md:tw-bg-transparent tw-duration-300 tw-ease-out ${
+              burger ? "tw-translate-x-0" : "tw-translate-x-full"
+            } md:tw-translate-x-0`}
+          >
+            <a
+              className="menu-link"
+              onClick={() => setBurger(false)}
+              href="#experiencia"
+            >
+              <li>Experiência</li>
+            </a>
+            <a
+              className="menu-link"
+              onClick={() => setBurger(false)}
+              href="#sobre"
+            >
+              <li>Sobre</li>
+            </a>
+            <a
+              className="menu-link"
+              onClick={() => setBurger(false)}
+              href="#habilidades"
+            >
+              <li>Habilidades</li>
+            </a>
+            <a
+              className="menu-link"
+              onClick={() => setBurger(false)}
+              href="#projetos"
+            >
+              <li>Projetos</li>
+            </a>
+            <a
+              className="menu-link"
+              onClick={() => setBurger(false)}
+              href="#contato"
+            >
+              <li>Contato</li>
+            </a>
+          </ul>
+          <Burger
+            className="tw-block md:tw-hidden"
+            onClick={() => setBurger(!burger)}
+            isOpen={burger}
+          />
+        </div>
+      </div>
+      <div className="tw-h-[70px] tw-w-full"></div>
+    </>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  prefix: "tw-",
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        theme: "#474A59",
+        transparent: "rgba(0,0,0,0)",
+        white: "#fff",
+        black: "#000",
+      },
+    },
   },
   plugins: [],
 };
