6d0bb87942812632eb0a3808f65d03603ed4803f
Pedro <web@agenciaade.com.br>
2022-10-22 19:32:22 -0300

    done/projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { IoMdClose } from 'react-icons/io'
 
 import Layout from './components/Layout';
 
@@ -6,6 +7,8 @@ import AOS from 'aos'
 import "aos/dist/aos.css"
 
 import HTMLReactParser from 'html-react-parser';
+
+import Modal from 'react-modal';
 
 type Experience = {
   title: string,
@@ -18,9 +21,25 @@ type About = {
   content: string
 }
 
+type Skill = {
+  title: string,
+  img: string,
+}
+
+type Project = {
+  img: string,
+  link: string,
+  title: string,
+  date: string,
+  skills: Skill[],
+  text: string
+}
+
 type Projects = {
   title: string,
   info_button: string,
+  project_link: string,
+  data: Project[]
 }
 
 interface DataType {
@@ -41,6 +60,10 @@ function App() {
   const [data, setData] = useState<DataType>(require('./db.json')[defaultLanguage]);
 
   const [experience, setExperience] = useState<Experience & {active: number}>({...data.experience[0], active: 0});
+  const [modal, setModal] = useState<{content: Project, active: boolean}>({
+    content: data.projects.data[0],
+    active: false,
+  })
 
   const skills = [
     {img: "/icons/Javascript.svg", name: "Javascript"},
@@ -54,13 +77,20 @@ function App() {
     {img: "/icons/Node.svg", name: "Node.js"},
   ]
 
-  const projects = [
-    {img: "/OrganoZero.png", link: "https://organozero.com.br"},
-    {img: "/Ibras.png", link: "https://organozero.com.br"},
-    {img: "/Sw-solar.png", link: "https://organozero.com.br"},
-    {img: "/Ggl.png", link: "https://organozero.com.br"},
-    {img: "/Golf.png", link: "https://organozero.com.br"},
-  ]
+  Modal.setAppElement("#root")
+  const customStyles = {
+    content: {
+      top: '90px',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, 0)',
+      maxWidth: "768px",
+      maxHeight: "calc(100vh - 110px)",
+    },
+    overflowY: "auto"
+  };
 
   useEffect(() => {
     AOS.init({
@@ -139,18 +169,57 @@ function App() {
       <div id="projetos" className='tw-px-[20px] tw-py-[100px] tw-mx-auto tw-w-full tw-max-w-[1024px]'>
           <h2>{data.projects.title}</h2>
           <div className='tw-w-full tw-flex tw-items-center tw-justify-center tw-flex-wrap tw-py-[50px]'>
-            {projects.map((p, i) => (
-              <a target="_blank" rel="noreferrer" 
-              href={p.link} 
+            {data.projects.data.map((p, i) => (
+              <button
+              onClick={() => setModal({active: true, content: p})}
               className='project tw-relative tw-w-[300px] tw-h-[300px] tw-overflow-hidden after:tw-content-[""] after:tw-w-full after:tw-h-full after:tw-absolute after:tw-top-0 after:tw-left-0 after:tw-bg-[rgba(0,0,0,.3)] after:tw-opacity-0 after:hover:tw-opacity-100 tw-duration-300 tw-ease-out'
               key={i}>
                 <div className='tw-w-[100px] tw-h-[100px] tw-absolute tw-top-[50%] tw-left-[50%] tw-translate-x-[-50%] tw-translate-y-[-50%] tw-bg-[#4F95D0] tw-rounded-full tw-flex tw-items-center tw-justify-center tw-pointer-events-none tw-z-[1] tw-opacity-0 tw-duration-300 tw-ease-out'>
                   <span className='tw-font-medium tw-text-[22px] tw-text-white tw-text-center tw-leading-[100%]' style={{fontFamily: "Nunito Sans"}}>{HTMLReactParser(data.projects.info_button)}</span>
                 </div>
                 <img className='tw-w-full tw-object-cover tw-duration-300 tw-ease-out' src={p.img} alt="" />
-              </a>
+              </button>
             ))}
           </div>
+          <Modal
+        isOpen={modal.active}
+        // onAfterOpen={afterOpenModal}
+        onRequestClose={() => setModal({...modal, active: false})}
+        style={customStyles}
+        contentLabel={modal.content.title}
+      >
+        <div className='tw-w-full tw-h-full tw-px-[20px] tw-py-[40px]'>
+          <div className='tw-flex tw-items-center tw-justify-end'>
+            <button className='hover:tw-cursor-pointer' onClick={() => setModal({...modal, active: false})}>
+              <IoMdClose size={40}  />
+            </button>
+          </div>
+          <div className='tw-flex sm:tw-items-center sm:tw-justify-center tw-flex-col sm:tw-flex-row tw-gap-[30px] tw-mb-[50px]'>
+          <a href={modal.content.link} target="_blank" rel="noreferrer">
+              <img className='tw-max-w-[300px] tw-w-full' src={modal.content.img} alt={modal.content.title} />
+            </a>
+
+            <div className='tw-flex tw-flex-col tw-align-start tw-justify-center'>
+              <h2 className='tw-text-left'>{modal.content.title}</h2>
+              <span>{modal.content.date}</span>
+              <div className="tw-flex tw-items-center tw-justify-start tw-gap-[10px] tw-py-[20px] tw-flex-wrap">
+                {modal.content.skills.map((s, i) => 
+                  <div key={i}>
+                    <img className='tw-w-[40px]' src={s.img} alt={s.title} />
+                  </div>
+                )}
+              </div>
+              <div>
+              <a className='tw-bg-[#4F95D0] tw-px-[30px] tw-rounded-full tw-py-[4px] tw-text-white tw-font-medium' href={modal.content.link} target="_blank" rel="noreferrer">{data.projects.project_link}</a>
+              </div>
+            </div>
+          </div>
+            <div>
+              {HTMLReactParser(modal.content.text)}
+            </div>
+        </div>
+        
+      </Modal>
       </div>
     </Layout>
   );
